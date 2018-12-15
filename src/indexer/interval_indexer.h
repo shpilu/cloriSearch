2794249e167a4404114407e6a6c7f3d4c9f7b429
shpilu <weijianlhp@163.com>
2018-12-15 21:23:33 +0800

    add interval indexer

--- src/indexer/interval_indexer.h
+++ src/indexer/interval_indexer.h
@@ -4,13 +4,12 @@
 // version: 1.0 
 // Copyright (C) 2018 James Wei (weijianlhp@163.com). All rights reserved
 //
-//
-//
 // TODO @James Wei
-// 1.  It's bad efficient to 目前跳跃表的插入效率不高, just rewrite skip_list 
+// 1.  It's bad efficient to 目前跳跃表的插入效率不高
 //
-//      2. 插入可能导致区间合并: e.g. Interval(10, 18] -- Docid(1, 2, 3, 4), Interval(18, 22] -- Docid(1, 2, 3) 在插入Interval(10, 22] -- Docid(4) 时应该合并区间为(10, 22] -- Docid(1, 2, 3, 4), 但目前没有合并区间
-//
+// 2. 插入可能导致区间合并: e.g. Interval(10, 18] -- Docid(1, 2, 3, 4), 
+// Interval(18, 22] -- Docid(1, 2, 3) 在插入Interval(10, 22] -- Docid(4) 
+// 时应该合并区间为(10, 22] -- Docid(1, 2, 3, 4), 但目前没有合并区间
 //
 // some tips-- 
 // 10-20 25-30
@@ -23,17 +22,16 @@
 #include <stdexcept>  // std::logic_error
 #include <type_traits> // std::is_same
 #include <functional> // std::less
-#include "third_party/skip_list/skip_list.h"
+#include "third_party/cloriskip/skip_list.h"
 #include "interval.h"
-
-#define INF 
-#define SUP
+#include "indexer.h"
 
 namespace cloris {
 
 template <typename T, typename Comp = std::less<T>>
-class IntervalNode : public Interval<T> {
+class IntervalNode : public Interval<T, Comp> {
 public:
+    operator bool() const { return this->is_empty() ? false: true; }
     bool operator < (const IntervalNode<T>& other) const { 
         if (this->is_empty() != other.is_empty()) {
             return this->is_empty();
@@ -45,56 +43,81 @@ public:
             }
         }
     }
-    IntervalNode(Term& term); 
-    Add(bool is_belong_to, int docid) { list_.Add(is_belong_to, docid); }
-    Interval<T> GetConjunction(IntervalNode<T>& other);
+    IntervalNode(const Term& term, ValueType type); 
+    IntervalNode(const Interval<T>& interval, const InvertedList& vlist);
+    IntervalNode(const Interval<T>& interval);
+    void Add(bool is_belong_to, int docid) { return list_.Add(is_belong_to, docid); }
+    const InvertedList& list() const { return list_; }
 private:
     InvertedList list_;
+};
+
+template<typename T, typename Comp>
+IntervalNode<T, Comp>::IntervalNode(const Interval<T>& interval, const InvertedList& vlist) {
+    this->Reset(interval);
+    list_.Copy(vlist);
 }
 
-// TODO @James Wei
-// Add compile_time type check
-// for string type
-template<typename T, typename A>
-IntervalNode::IntervalNode(Term& term) {
-    if (!(type_ & term.type() & BASIC_TYPE_MASK)) {
-        type_ = IntervalType::BAD;
-        return;
-    }
-    // if term is a interval type
-    if ((type_ & term.type()) & INTERVAL_TYPE_MASK) {
-        flag_ = term.flag(); 
-        left_ = *reinterpret_cast<T*>(term.left());
-        right_ = *reinterpret_cast<T*>(term.right());
-    } else {
-        // term is not a interval type, extend it to [X, X]
-        left_ = *reinterpret_cast<T*>(term.value()[0]);
-        right_ = *reinterpret_cast<T*>(term.value()[0]);
-        flag_ = IntervalType::CLOSE;
-    }
+template<typename T, typename Comp>
+IntervalNode<T, Comp>::IntervalNode(const Interval<T>& interval) {
+    this->Reset(interval);
+}
+
+//
+// template specialization for support data type
+//
+// string
+template<typename T>
+T convert(const void *p, size_t sz) {
+    return T(static_cast<const char*>(p), sz);
+}
+
+template<>
+int32_t convert(const void *p, size_t sz) {
+    return *reinterpret_cast<const int32_t*>(p);
+}
+
+template<>
+double convert(const void *p, size_t sz) {
+    return *reinterpret_cast<const double*>(p);
+}
+
+// string
+template<typename T>
+T convert(const void *p) {
+    return T(static_cast<const char*>(p));
+}
+
+template<>
+int32_t convert(const void *p) {
+    return *reinterpret_cast<const int32_t*>(p);
+}
+
+template<>
+double convert(const void *p) {
+    return *reinterpret_cast<const double*>(p);
 }
 
 // special for string type
-template<std::string, typename A>
-IntervalNode::IntervalNode(Term& term) {
-    if (!(type_ & term.type() & BASIC_TYPE_MASK)) {
-        type_ = IntervalType::BAD;
+template<typename T, typename Comp>
+IntervalNode<T, Comp>::IntervalNode(const Term& term, ValueType type) {
+    if (!(type & term.type() & BASIC_TYPE_MASK)) {
+        this->set_empty();
         return;
     }
     // if term is a interval type
-    if ((type_ & term.type()) & INTERVAL_TYPE_MASK) {
-        flag_ = term.flag(); 
+    if ((type & term.type()) & INTERVAL_TYPE_MASK) {
         size_t left_size;
         size_t right_size;
-        void *lptr = term.left(&left_size);
-        void *rptr = term.right(&right_size);
-        left_.assign(reinterpret_cast<char*>(lptr), left_size);
-        right_.assign(reinterpret_cast<char*>(rptr), right_size);
+        const void *lptr = term.left(&left_size);
+        const void *rptr = term.right(&right_size);
+        T left = convert<T>(lptr, left_size);
+        T right = convert<T>(rptr, right_size);
+        this->Reset(left, right, term.flag());
     } else {
         // if term is not a interval type, extend it to [X, X]
-        left_ = term.value();
-        right_ = term.value();
-        flag_ = IntervalType::CLOSE;
+        const void *ptr = term.data();
+        this->Reset(convert<T>(ptr), convert<T>(ptr), INTERVAL_CLOSE_FLAG);
     }
 }
 
@@ -104,6 +127,7 @@ public:
     IntervalIndexer(const std::string& name);
     ~IntervalIndexer();
     bool ParseTermsFromConjValue(std::vector<Term>& terms, const ConjValue& value);
+    bool Add(const Term& term, bool is_belong_to, int docid);
     virtual bool Add(const ConjValue& value, bool is_belong_to, int docid);
     virtual const std::list<DocidNode>* GetPostingLists(const Term& term);
 private:
@@ -111,23 +135,27 @@ private:
 };
 
 template<typename T, typename C>
-IntervalIndexer<T, C>::IntervalIndexer(const std::string& name) 
-    : name_(name) {
-    if (std::is_same(T, int32_t)) {
-        type_ = INT32_INTERVAL;
-    } else if (std::is_same(T, float)) {
-        type_ = DOUBLE_INTERVAL;
-    } else if (std::is_same(T, std::string)) {
-        type_ == STRING_INTERVAL;
-    } else {
+IntervalIndexer<T, C>::IntervalIndexer(const std::string& name) : Indexer(name) {
+    #define INT32_OFFSET        1
+    #define DOUBLE_OFFSET       2
+    #define STRING_OFFSET       3
+    int32_t flag(0x01);
+    flag <<= std::is_same<T, int32_t>() * INT32_OFFSET;
+    flag <<= std::is_same<T, double>() * DOUBLE_OFFSET;
+    flag <<= std::is_same<T, std::string>() * STRING_OFFSET;
+    flag |= INTERVAL_TYPE_MASK;
+
+    if (flag < MIN_INTERVAL_TYPE || flag > MAX_INTERVAL_TYPE) {
         throw std::logic_error("unsupported interval indexer type");
+    } else {
+        type_ = static_cast<ValueType>(flag);
     }
 }
 
 template<typename T, typename C>
 IntervalIndexer<T, C>::~IntervalIndexer() {}
 
-// TODO
+// TODO support single value, e.g. age ∈ {18}
 template<typename T, typename C>
 bool IntervalIndexer<T, C>::ParseTermsFromConjValue(std::vector<Term>& terms, const ConjValue& value) {
     if (type_ == INT32_INTERVAL) {
@@ -146,17 +174,18 @@ bool IntervalIndexer<T, C>::ParseTermsFromConjValue(std::vector<Term>& terms, co
     return true;
 }
 
-// DDX
+// TODO support range search like 18 <= age < 20, not only single value
 template<typename T, typename C>
 const std::list<DocidNode>* IntervalIndexer<T, C>::GetPostingLists(const Term& term) {
-    IntervalNode<T> search_node(term);
-    if (!search_node()) {
+    IntervalNode<T> search_node(term, type_);
+    if (!search_node) {
         return NULL;
     }
     // 得到实际是交集
-    goodliffe::skip_list<IntervalNode<T>>::iterator iter = inverted_lists_.find(search_node);
-    if (iter) {
-        return iter->posting_list();
+    typename goodliffe::skip_list<IntervalNode<T, C>>::iterator iter = inverted_lists_.find(search_node);
+    if (iter != inverted_lists_.end()) {
+        const InvertedList& li = iter->list();
+        return &(li.doc_list());
     } else {
         return NULL;
     }
@@ -167,52 +196,49 @@ const std::list<DocidNode>* IntervalIndexer<T, C>::GetPostingLists(const Term& t
 //
 template<typename T, typename C>
 bool IntervalIndexer<T, C>::Add(const Term& term, bool is_belong_to, int docid) {
-    IntervalNode<T> search_node(term);
-    // 找到第一个有交集的点
-    while (search_node()) {
-        goodliffe::skip_list<IntervalNode<T>, C>::iterator iter = inverted_lists_.find(search_node);
-        // 没有交集
-        if (!iter) {
+    IntervalNode<T> search_node(term, type_);
+    while (search_node) {
+        // try to find the the first node whose intersection with term is not empty 
+        typename goodliffe::skip_list<IntervalNode<T, C>>::iterator iter = inverted_lists_.find(search_node);
+        // empty intersection 
+        if (iter != inverted_lists_.end()) {
             // TODO, check...
             search_node.Add(is_belong_to, docid);
             inverted_lists_.insert(search_node);
+            break;
         } else {
-            // old, new,
-            // 计算交集
-            Interval<T> intvl_conj = iter->GetConjunction(search_node);
-            std::pair<Interval<T>, Interval<T>> ivl = iter->CutSubset(intvl_conj); // left
-            if (!ival.first() && !ival.second()) {
-                // 新的完全包含旧的
+            Interval<T> left, intersection, right, xleft, xright;
+            iter->Slice(search_node, left, intersection, right); 
+            search_node.Subtract(*iter, xleft, xright);
+            if (!left && !right) {
+                // the old is absolutely covered 
                 iter->Add(is_belong_to, docid);
             } else {
-                IntervalNode<T> node(intvl_conj, *iter);
+                InvertedList ilist;
+                ilist.Copy(iter->list());
+                IntervalNode<T> node(intersection, ilist);
                 inverted_lists_.erase(iter);
-                
                 node.Add(is_belong_to, docid);
-
                 inverted_lists_.insert(node);
 
-                if (ival.first()) {
-                    ival.first.copy(*iter);
-                    inverted_lists_.insert(ival.first);
+                if (left) {
+                    IntervalNode<T> left_node(left, ilist);
+                    inverted_lists_.insert(left_node);
                 }
-                if (ival.second()) {
-                    ival.second.copy(*iter)
-                    inverted_lists_.insert(ival.second);
+                if (right) {
+                    IntervalNode<T> right_node(left, ilist);
+                    inverted_lists_.insert(right_node);
                 }
             }
-            std::pair<Interval<T>, Interval<T>> xival = search_node.CutSubset(intvl_conj);
-            if (xival > ZERO) {
-                // 插左结点
+            if (xleft) {
+                IntervalNode<T> xleft_node(xleft);
+                xleft_node.Add(is_belong_to, docid);
+                inverted_lists_.insert(xleft_node);
             }
-            search_node = right(search_node - iter);
-            goto next_loop;
-            
+            search_node = xright;
         }
     }
-    if (search_node > ZERO) {
-        inverted_lists_.insert(is_belong_to, docid);
-    }
+    return true;
 }
 
 // [10, 18), [20, 30)
@@ -222,8 +248,11 @@ bool IntervalIndexer<T, C>::Add(const ConjValue& value, bool is_belong_to, int d
     this->ParseTermsFromConjValue(terms, value);
     for (auto &term : terms) {
         // [10, 18)
-        = this->Add(term, is_belong_to, docid);
+        if (!this->Add(term, is_belong_to, docid)) {
+            return false;
+        }
     }
+    return true;
 }
 
 } // namespace cloris

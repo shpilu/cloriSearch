d6a7b2b4bc7f98b732956777a0f9683aea2104ed
shpilu <weijianlhp@163.com>
2018-12-15 21:22:45 +0800

    update interval description and implementation

--- src/indexer/interval.h
+++ src/indexer/interval.h
@@ -27,36 +27,56 @@
 
 #define INTERVAL_RIGHT_OPEN     0x00000001 // )
 #define INTERVAL_CLOSE_MASK     0x00000002 // [ or ]
+#define INTERVAL_CLOSE          0x00000002
 #define INTERVAL_LEFT_OPEN      0x00000004 // (
 
+// Juset used in 'Reset' method
+#define INTERVAL_CLOSE_FLAG     0x00000003
+
 namespace cloris {
+
+enum Dir {
+    LEFT = 0,
+    RIGHT = 1,
+};
 
 template<typename T>
 struct BoundaryPoint {
     BoundaryPoint() : flag(INTERVAL_EMPTY) {}
-    bool operator < (const BoundaryPoint& other) { 
+    BoundaryPoint(T _value, uint8_t _flag) : value(_value), flag(_flag) {}
+    bool operator == (const BoundaryPoint& other) const {
+        if (flag & INTERVAL_EMPTY) {
+            return other.flag & INTERVAL_EMPTY;
+        } else {
+            return ((value == other.value) && (flag == other.flag));
+        }
+    }
+    bool operator < (const BoundaryPoint& other) const { 
         if (flag & INTERVAL_EMPTY) {
             return (other.flag & INTERVAL_EMPTY) ? false : true;
         } else {
             if (other.flag & INTERVAL_EMPTY) {
-                retruen false;
+                return false;
             } else {
                 return (value < other.value) || ((value == other.value) && (flag < other.flag)); 
             }
         }
     }
+    bool operator <= (const BoundaryPoint& other) const {
+        return (operator < (other)) || (operator == (other));
+    }
     //
     // weight: ( ==> 4, [ ==> 2, ] ==> 2, ) ==> 1
     //
     // 
-    BoundaryPoint get_inverted(Dir dir) {
-        uint8_t flag;
+    BoundaryPoint GetInverted(Dir dir) const {
+        uint8_t new_flag;
         if (flag & INTERVAL_CLOSE_MASK) {
-            flag = (dir == RIGHT) ? INTERVAL_LEFT_OPEN : INTERVAL_RIGHT_OPEN;
+            new_flag = (dir == RIGHT) ? INTERVAL_LEFT_OPEN : INTERVAL_RIGHT_OPEN;
         } else {
-            flag = INTERVAL_CLOSE_MASK;
+            new_flag = INTERVAL_CLOSE;
         }
-        return std::move(BoundaryPoint(value, flag));
+        return BoundaryPoint(value, new_flag);
     }
     T value;
     uint8_t flag;
@@ -66,18 +86,30 @@ template <typename T, typename Compare = std::less<T>>
 class Interval {
     typedef Interval<T, Compare> SelfType;
     typedef BoundaryPoint<T> Point; 
+    static Interval MakeInterval(const Point& left, const Point& right) { 
+        return Interval(left, right);  
+    }
 public:
-    SelfType() {}
-    ~SelfType() {}
-    MakeInterval(const Point& left, const Point& right) : left_(left), right_(right) {}
-    inline uint8_t flag() const { return left_.flag | right_.flag(); }
-    bool operator() { return !(flag() & INTERVAL_EMPTY); }
-    void Subtract(const SelfType& target, SelfType& left_result, SelfType& right_result);
-    void Intersect(const SelfType& target, SelfType& result); 
+    Interval(const Point& left, const Point& right) : left_(left), right_(right) {}
+    Interval() {}
+    ~Interval() {}
+    inline void Reset(const SelfType& other) { *this = other; } 
+    inline void Reset(const T& left, const T& right, int32_t flag) {
+        left_.value = left;
+        left_.flag = (flag & INTERVAL_CLOSE_MASK) ? INTERVAL_CLOSE : INTERVAL_LEFT_OPEN;
+        right_.value = right;
+        right_.flag = (flag & INTERVAL_CLOSE_MASK) ? INTERVAL_CLOSE : INTERVAL_RIGHT_OPEN;
+    }
+    operator bool() const { return !(flag() & INTERVAL_EMPTY); }
+
+    inline uint8_t flag() const { return left_.flag | right_.flag; }
+    void Subtract(const SelfType& target, SelfType& left_result, SelfType& right_result) const;
+    void Slice(const SelfType& target, SelfType& left_result, SelfType& intersection, SelfType& right_result) const;
+    void Intersect(const SelfType& target, SelfType& result) const; 
     inline const Point& left() const { return left_; }
     inline const Point& right() const { return right_; }
     inline bool is_empty() const { return flag() & INTERVAL_EMPTY; }
-    inline void set_empty() const { 
+    inline void set_empty() { 
         left_.flag |= INTERVAL_EMPTY; 
         right_.flag |= INTERVAL_EMPTY; 
     }
@@ -87,14 +119,14 @@ private:
 };
 
 template <typename T, typename Compare>
-void Interval::Subtract(const SelfType& target, SelfType& left_result, SelfType& right_result) {
+void Interval<T, Compare>::Subtract(const SelfType& target, SelfType& left_result, SelfType& right_result) const {
     if (this->is_empty()) {
         left_result.set_empty();
         right_result.set_empty();
         return;
     }
     // we treat empty interval as the smallest interval
-    if (target->is_empty()) {
+    if (target.is_empty()) {
         left_result.set_empty();
         right_result = *this; 
         return;
@@ -117,7 +149,7 @@ void Interval::Subtract(const SelfType& target, SelfType& left_result, SelfType&
         } else {
             // case B
             if (target.right() < right()) {
-                right_result = MakeInterval(target.right().get_inverted(DIR::Right), right());
+                right_result = SelfType::MakeInterval(target.right().GetInverted(Dir::RIGHT), right());
             } else {
                 // case C
                 right_result.set_empty();
@@ -126,12 +158,12 @@ void Interval::Subtract(const SelfType& target, SelfType& left_result, SelfType&
     } else {
         // case D
         if (target.right() < right()) {
-            left_result = MakeInterval(left(), target.left().get_inverted(DIR::Left));
-            right_result = MakeInterval(target.right().get_inverted(DIR::Right), right());
+            left_result = SelfType::MakeInterval(left(), target.left().GetInverted(Dir::LEFT));
+            right_result = SelfType::MakeInterval(target.right().GetInverted(Dir::RIGHT), right());
         } else {
             // case E
-            if (b.left() <= right()) {
-                left_result = MakeInterval(left(), target.left().get_inverted(DIR::Left));
+            if (target.left() <= right()) {
+                left_result = SelfType::MakeInterval(left(), target.left().GetInverted(Dir::LEFT));
                 right_result.set_empty();
             } else {
                 // case F
@@ -143,7 +175,7 @@ void Interval::Subtract(const SelfType& target, SelfType& left_result, SelfType&
 }
 
 template <typename T, typename Compare>
-void Interval::Intersect(const SelfType& target, SelfType& result) {
+void Interval<T, Compare>::Intersect(const SelfType& target, SelfType& result) const {
     if (is_empty() || target.is_empty()) {
         result.set_empty();
         return;
@@ -163,7 +195,7 @@ void Interval::Intersect(const SelfType& target, SelfType& result) {
         } else {
             // case B
             if (target.right() < right()) {
-                result = MakeInterval(left(), target.right());
+                result = SelfType::MakeInterval(left(), target.right());
             } else {
                 // case C
                 result = *this;
@@ -175,11 +207,68 @@ void Interval::Intersect(const SelfType& target, SelfType& result) {
             result = target;
         } else {
             // case E
-            if (b.left() <= right()) {
-                result = MakeInterval(target.left(), right());
+            if (target.left() <= right()) {
+                result = SelfType::MakeInterval(target.left(), right());
             } else {
                 // case F
                 result.set_empty();
+            }
+        }
+    }
+}
+
+// the combination of Subtract and Intersect
+template <typename T, typename Compare>
+void Interval<T, Compare>::Slice(const SelfType& target, 
+        SelfType& left_result, SelfType& intersection, SelfType& right_result) const {
+    if (this->is_empty()) {
+        left_result.set_empty();
+        intersection.set_empty();
+        right_result.set_empty();
+        return;
+    }
+    // we treat empty interval as the smallest interval
+    if (target.is_empty()) {
+        left_result.set_empty();
+        intersection.set_empty();
+        right_result = *this; 
+        return;
+    }
+
+    if (target.left() < left()) {
+        left_result.set_empty();
+        // case A
+        if (target.right() < left()) {
+            intersection.set_empty();
+            right_result = *this;
+        } else {
+            // case B
+            if (target.right() < right()) {
+                right_result = SelfType::MakeInterval(target.right().GetInverted(Dir::RIGHT), right());
+                intersection = SelfType::MakeInterval(left(), target.right());
+            } else {
+                // case C
+                right_result.set_empty();
+                intersection = *this;
+            }
+        }
+    } else {
+        // case D
+        if (target.right() < right()) {
+            left_result = SelfType::MakeInterval(left(), target.left().GetInverted(Dir::LEFT));
+            intersection = target;
+            right_result = SelfType::MakeInterval(target.right().GetInverted(Dir::RIGHT), right());
+        } else {
+            // case E
+            if (target.left() <= right()) {
+                left_result = SelfType::MakeInterval(left(), target.left().GetInverted(Dir::LEFT));
+                intersection = SelfType::MakeInterval(target.left(), right());
+                right_result.set_empty();
+            } else {
+                // case F
+                left_result = *this;
+                right_result.set_empty();
+                intersection.set_empty();
             }
         }
     }

100f8c416c7e2f850c3daef424f0b714f40e2b38
shpilu <weijianlhp@163.com>
2018-12-16 10:44:10 +0800

    update factory pattern implementation

--- src/indexer/indexer_factory.cc
+++ src/indexer/indexer_factory.cc
@@ -9,6 +9,14 @@
 #include "interval_indexer.h"
 #include "indexer_factory.h"
 
+#define INDEX_TYPE_SIMPLE       "simple"
+#define INDEX_TYPE_INTERVAL     "interval"
+#define INDEX_TYPE_GEO          "geo"
+
+#define KEY_TYPE_INT32          "int32"
+#define KEY_TYPE_DOUBLE         "double"
+#define KEY_TYPE_STRING         "string"
+
 namespace cloris {
 
 IndexerFactory* IndexerFactory::instance() noexcept {
@@ -16,20 +24,20 @@ IndexerFactory* IndexerFactory::instance() noexcept {
 }
 
 Indexer* IndexerFactory::CreateIndexer(const std::string& name, const std::string& key_type, const std::string& index_type) {
-    if (index_type == "simple") {
-        if (key_type == "int32") {
+    if (index_type == INDEX_TYPE_SIMPLE) {
+        if (key_type == KEY_TYPE_INT32) {
             return new SimpleIndexer(name, ValueType::INT32);
-        } else if (key_type == "string") {
+        } else if (key_type == KEY_TYPE_STRING) {
             return new SimpleIndexer(name, ValueType::STRING);
         } else {
             return NULL;
         }
-    } else if (index_type == "interval") {
-        if (key_type == "int32") {
+    } else if (index_type == INDEX_TYPE_INTERVAL) {
+        if (key_type == KEY_TYPE_INT32) {
             return new IntervalIndexer<int32_t>(name);
-        } else if (key_type == "double") {
+        } else if (key_type == KEY_TYPE_DOUBLE) {
             return new IntervalIndexer<double>(name);
-        } else if (key_type == "string") {
+        } else if (key_type == KEY_TYPE_STRING) {
             return new IntervalIndexer<std::string>(name);
         } else {
             return NULL;

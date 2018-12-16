8b8cdc296fec4148a3c11ffc995c063f8ecef9c5
shpilu <weijianlhp@163.com>
2018-12-16 10:56:11 +0800

    update test script

--- bin/run.sh
+++ bin/run.sh
@@ -4,4 +4,17 @@ cd `dirname $0`
 dir=$(pwd)
 
 ulimit -c unlimited
-${dir}/tutorial
+
+if [ $# -lt 1 ]
+then
+    ${dir}/tutorial_simple
+elif [ "$1" == "simple" ]
+then
+    ${dir}/tutorial_simple
+elif [ "$1" == "interval" ]
+then
+    ${dir}/tutorial_interval
+else 
+    echo "unknown command, just 'simple/interval/geo' is supported"
+fi
+

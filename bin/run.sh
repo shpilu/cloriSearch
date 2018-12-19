6233ce059c30c44d3c0714a48d19a18456db43a4
shpilu <weijianlhp@163.com>
2018-12-19 23:55:13 +0800

    add geo indexer test case

--- bin/run.sh
+++ bin/run.sh
@@ -14,6 +14,9 @@ then
 elif [ "$1" == "interval" ]
 then
     ${dir}/tutorial_interval
+elif [ "$1" == "geo" ]
+then
+    ${dir}/tutorial_geo
 else 
     echo "unknown command, just 'simple/interval/geo' is supported"
 fi
---
+++ src/example/tutorial_geo.cc
@@ -0,0 +1,63 @@
+//
+// cloriSearch tutorial 
+//
+
+#include <fstream>
+#include <iostream>
+#include "jsoncpp/json_util.h"
+#include "clorisearch.h"
+
+using namespace cloris;
+
+const char* g_index_schema = "{ \
+    \"terms\":[{ \
+        \"name\":\"location\", \
+        \"key_type\":\"double\", \
+        \"index_type\":\"geo\" \
+    }] \
+}";
+
+
+int main() {
+    CloriSearch *sch = new CloriSearch();
+    if (!sch->Init(g_index_schema, IndexSchemaFormat::ISF_JSON, SourceType::DIRECT)) {
+        std::cout << "cloriSearch init failed" << std::endl;
+    } else {
+        std::cout << "cloriSearch init success" << std::endl;
+    }
+
+    for (int i = 1; i <= 3; ++i) {
+        std::string key = std::string("../conf/geo_index_test/index_") + std::to_string(i) + ".json";
+        std::cout << key << std::endl;
+            
+        std::fstream in(key.c_str());
+        std::istreambuf_iterator<char> begin(in); 
+        std::istreambuf_iterator<char> end; 
+        std::string dnf_str(begin, end);
+
+        if (sch->Add(dnf_str, IndexSchemaFormat::ISF_JSON, false)) {
+            std::cout << "add index success" << std::endl;
+        } else {
+            std::cout << "add index failed, dnf_str=" << dnf_str << std::endl;
+        }
+    }
+
+    Query query;
+    query["city"] = "shanghai";
+    query["age"] = 16;
+    query["gendor"] = "female";
+    query["student"] = true;
+    query["location"] = GeoRange(116.400693, 39.907688, 20000);
+
+    for (auto &p : query) {
+        std::cout << "[PARAMS]" << p.print() << std::endl;
+    }
+
+    std::vector<int> res = sch->Search(query, 10);
+    std::cout << "search result, size=" << res.size() << std::endl;
+
+    for (auto &p : res) {
+        std::cout << "docid=" << p << std::endl;
+    }
+    return 0;
+}
